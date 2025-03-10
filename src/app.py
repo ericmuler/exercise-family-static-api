7bab61ade17e1eeb5fc1cdfc50327b7301a3eb4d
Eric <ericmulerofernandez@gmail.com>
2025-03-10 17:08:19 +0000

    Creo que ahora si...

--- src/app.py
+++ src/app.py
@@ -6,39 +6,84 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
+initial_members = [
+    {
+        "id": 1,
+        "first_name": "John",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+    },
+    {
+        "id": 2,
+        "first_name": "Jane",
+        "age": 35,
+        "lucky_numbers": [10, 14, 3]
+    },
+    {
+        "id": 3,
+        "first_name": "Jimmy",
+        "age": 5,
+        "lucky_numbers": [1, 14, 78]
+    }
+]
+
+for member in initial_members:
+    jackson_family.add_member(member)
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
+
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
 
-# this only runs if `$ python src/app.py` is executed
+    member = jackson_family.get_member(id)
+
+    return jsonify({"message": "here is the member"}, member)
+
+
+@app.route('/members', methods=['POST'])
+def new_member():
+
+    the_new_member = request.json
+
+    jackson_family.add_member(the_new_member)
+
+    return jsonify({"message": "Member added successfully"}), 200
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+
+    success = jackson_family.delete_member(id)
+
+    if not success:
+        return jsonify({"message": "Member not found"}), 404
+
+    return jsonify({"message": "Member deleted successfully"}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,29 +8,46 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
+        self.last_name = last_name
+        self._next_id = 1
         self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+
+    def _generate_id(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+
+        if 'id' not in member:
+            member['id'] = self._generate_id()
+
+        member['last_name'] = self.last_name
+
+        self._members.append(member)
+
+        return self._members
+
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        
+        for i, member in enumerate(self._members):
+            if member['id'] == id:
+                del self._members[i]
+                return True
+        return False
+
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
 
-    # this method is done, it returns a list with all the family members
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
+
     def get_all_members(self):
         return self._members
